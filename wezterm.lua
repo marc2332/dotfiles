5175a00e40b77abb6c4cb08e2143df8c5a517a24
marc2332 <mespinsanz@gmail.com>
2023-01-31 21:28:21 +0100

    first commit

---
+++ wezterm.lua
@@ -0,0 +1,121 @@
+local wezterm = require('wezterm')
+
+return {
+  enable_scroll_bar = true,
+  default_prog = { "nu" },
+  color_scheme = "Gruvbox Dark",
+  use_fancy_tab_bar = true,
+  font = wezterm.font("JetBrains Mono", { weight = 'Bold' }),
+  window_padding = {
+    left = 24,
+    right = 24,
+    top = 24,
+    bottom = 10,
+  },
+  window_decorations = "RESIZE",
+  tab_max_width = 24,
+  colors = {
+    background = '#282828',
+  },
+  keys = {
+    {
+      key = '-',
+      mods = 'ALT',
+      action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+    {
+      key = '+',
+      mods = 'ALT',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+	{
+	  key = 'ç',
+	  mods = 'ALT',
+	  action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+	},
+    {
+      key = 'RightArrow',
+      mods = 'ALT|SHIFT',
+      action = wezterm.action.ActivateKeyTable {
+        name = 'resize_pane',
+        one_shot = false,
+      },
+    },
+    {
+      key = 'LeftArrow',
+      mods = 'ALT|SHIFT',
+      action = wezterm.action.ActivateKeyTable {
+        name = 'resize_pane',
+        one_shot = false,
+      },
+    },
+    {
+      key = 'LeftArrow',
+      mods = 'ALT',
+      action = wezterm.action.ActivateKeyTable {
+        name = 'activate_pane',
+        one_shot = false,
+      },
+    },
+    {
+      key = 'RightArrow',
+      mods = 'ALT',
+      action = wezterm.action.ActivateKeyTable {
+        name = 'activate_pane',
+        one_shot = false,
+      },
+    },
+  },
+
+  key_tables = {
+    resize_pane = {
+      {
+        key = 'RightArrow',
+        mods = 'ALT|SHIFT',
+        action = wezterm.action.AdjustPaneSize { 'Right', 3 },
+      },
+      {
+        key = 'LeftArrow',
+        mods = 'ALT|SHIFT',
+        action = wezterm.action.AdjustPaneSize { 'Left', 3 },
+      },
+	  {
+        key = 'UpArrow',
+        mods = 'ALT|SHIFT',
+        action = wezterm.action.AdjustPaneSize { 'Up', 3 },
+      },
+      {
+        key = 'DownArrow',
+        mods = 'ALT|SHIFT',
+        action = wezterm.action.AdjustPaneSize { 'Down', 3 },
+      }
+
+    },
+    activate_pane = {
+      {
+        key = 'RightArrow',
+        mods = 'ALT',
+        action = wezterm.action.ActivatePaneDirection 'Right',
+      },
+      {
+        key = 'LeftArrow',
+        mods = 'ALT',
+        action = wezterm.action.ActivatePaneDirection 'Left' ,
+      },
+	  {
+        key = 'UpArrow',
+        mods = 'ALT',
+        action = wezterm.action.ActivatePaneDirection 'Up',
+      },
+      {
+        key = 'DownArrow',
+        mods = 'ALT',
+        action = wezterm.action.ActivatePaneDirection 'Down' ,
+      }
+    },
+  },
+  inactive_pane_hsb = {
+    saturation = 1,
+    brightness = 1,
+  }
+}
